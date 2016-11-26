f930bec86883e15c3353427421ed907014d750e8
Alessandro Marzialetti <setti93@gmail.com>
2016-11-26 21:57:22 +0100

    First commit

---
+++ get_next_line.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/26 20:10:49 by amarzial          #+#    #+#             */
+/*   Updated: 2016/11/26 20:50:00 by amarzial         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "../libft/libft.h"
+#include "get_next_line.h"
+
+int get_next_line(const int fd, char **line)
+{
+	static char		*buffer;
+
+
+}
