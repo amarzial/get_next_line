94933d83b67d5dd7f3e19379ddfae6d15904a92f
Alessandro Marzialetti <setti93@gmail.com>
2016-11-27 16:27:02 +0100

    sketch

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/26 20:10:49 by amarzial          #+#    #+#             */
-/*   Updated: 2016/11/26 20:50:00 by amarzial         ###   ########.fr       */
+/*   Updated: 2016/11/27 16:26:44 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,16 @@
 
 int get_next_line(const int fd, char **line)
 {
-	static char		*buffer;
+	static char		buffer[BUFF_SIZE];
+	static char		*stop;
+	int				rb;
 
-
+	if (!(*line = (char*)malloc(sizeof(char) * BUFF_SIZE)))
+		return (0);
+	while ((rb = read(fd, buffer, BUFF_SIZE)) > 0)
+	{
+		if((stop = ft_memccpy(*line, buffer, BUFF_SIZE)))
+			
+	}
+	if (ft_memchr(buffer, '\n', BUFF_SIZE))
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,14 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/26 20:11:00 by amarzial          #+#    #+#             */
-/*   Updated: 2016/11/26 20:11:07 by amarzial         ###   ########.fr       */
+/*   Updated: 2016/11/27 16:26:46 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
 
-
+int get_next_line(const int fd, char **line);
 
 #endif
