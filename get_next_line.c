4596d2ed839840aaf1142fe0c73e82c2eb192974
Alessandro Marzialetti <setti93@gmail.com>
2016-11-29 15:52:30 +0100

    progress?

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amarzial <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/26 20:10:49 by amarzial          #+#    #+#             */
-/*   Updated: 2016/11/28 17:04:37 by amarzial         ###   ########.fr       */
+/*   Updated: 2016/11/29 15:44:30 by amarzial         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,41 @@
 #include "libft.h"
 #include "get_next_line.h"
 
-int	buffalloc(char **buff,char **start, int *size)
+void	resetbuff(t_reader *rdr)
+{
+	int		size;
+
+	if (!rdr->buffer)
+		return ;
+	size = rdr->size - (rdr->eol - rdr->buffer + 1);
+	ft_memmove(rdr->buffer, rdr->eol + 1, size);
+	rdr->start = rdr->buffer + size;
+	ft_memset(rdr->start, 0, rdr->size - size);
+}
+
+int		buffalloc(char **buff,char **start, int *size)
 {
 	char	*tmp;
 
-	*start = *buff + *size;
 	if (!(tmp = (char*)malloc(*size + BUFF_SIZE)))
 		return (0);
 	ft_memcpy(tmp, *buff, *size);
+	*start = tmp + *size;
 	*size += BUFF_SIZE;
 	ft_memdel((void**)buff);
 	*buff = tmp;
 	return (1);
 }
 
-int	get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
 	static t_reader	rdr;
 	int				cnt;
 
+	resetbuff(&rdr);
 	if (!rdr.buffer && !(rdr.buffer = (char*)malloc(BUFF_SIZE)))
 		return (-1);
-	while (!(rdr.eol = ft_memchr(rdr.start, '\n', rdr.size)) && !rdr.stop)
+	while (!(rdr.eol = ft_memchr(rdr.buffer, '\n', rdr.size)) && !rdr.stop)
 	{
 		if (!buffalloc(&rdr.buffer, &rdr.start, &rdr.size))
 			return (-1);
@@ -46,8 +59,6 @@ int	get_next_line(const int fd, char **line)
 		if (cnt < BUFF_SIZE)
 			rdr.stop = rdr.start + cnt;
 	}
-	ft_putptr(rdr.eol);
-	ft_putchar('\n');
 	rdr.eol = (rdr.eol) ? rdr.eol : rdr.stop;
 	if (!(*line = (char*)ft_strnew(rdr.eol - rdr.buffer)))
 		return (-1);
